c1832c1d6b4f6f1244098eefc74ca0838eab4a76
Zihao Tao <taozihao@uw.edu>
2019-03-25 13:33:58 -0700

    ss

--- src/firstMissingPositive.java
+++ src/firstMissingPositive.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class firstMissingPositive {
     public int firstMissingPositive(int[] nums) {
@@ -29,8 +31,44 @@ public class firstMissingPositive {
         return -1;
     }
 
+    public int firstMissingPositive2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int max = 0;
+        for(int i : nums) {
+            if(i > 0) {
+                set.add(i);
+                max = Math.max(max, i);
+            }
+        }
+        for(int i = 1; i <= max; i++) {
+            if(!set.contains(i)) {
+                return i;
+            }
+        }
+        return max + 1;
+    }
+
+    public int firstMissingPositive3(int[] nums) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) return i + 1;
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,2,1,3,1,0,4,0};
-        System.out.println((new firstMissingPositive()).firstMissingPositive(nums));
+        int[] nums = {3,4,-1,1};
+        System.out.println((new firstMissingPositive()).firstMissingPositive3(nums));
     }
 }
